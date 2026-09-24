b7547f8546dc9bdd382460e205afc4f3cec86ce5
Ivan Bozhanov <git@vakata.com>
2026-09-24 13:22:29 +0300

    keyd

--- local/install.sh
+++ local/install.sh
@@ -20,6 +20,21 @@ backup_and_link() {
     ln -s "$src" "$dest"
 }
 
+sudo_backup_and_link() {
+    src="$1"
+    dest="$2"
+
+    if [ -e "$dest" ] || [ -L "$dest" ]; then
+        backup="${dest}.bak.$STAMP"
+
+        echo "Backing up $dest -> $backup"
+        sudo mv "$dest" "$backup"
+    fi
+
+    echo "Linking $dest -> $src"
+    sudo ln -s "$src" "$dest"
+}
+
 # Top-level dotfiles
 for src in "$DIR"/.*; do
     name="$(basename "$src")"
@@ -51,3 +66,19 @@ if [ -d "$DIR/.config" ]; then
         backup_and_link "$src" "$HOME/.config/$name"
     done
 fi
+
+
+# etc entries
+for src in "$DIR/etc"/* "$DIR/etc"/.*; do
+    [ -e "$src" ] || [ -L "$src" ] || continue
+
+    name="$(basename "$src")"
+
+    case "$name" in
+        .|..)
+            continue
+            ;;
+    esac
+
+    sudo_backup_and_link "$src" "/etc/$name"
+done
